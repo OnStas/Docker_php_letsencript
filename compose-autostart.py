9a7431c8199d830c38122df4af3720049f10e817
OnStas <stanislav-helios@i.ua>
2021-04-12 17:12:17 +0300

    Update compose-autostart.py
    Add if...else for 400...500 requests

--- compose-autostart.py
+++ compose-autostart.py
@@ -4,14 +4,22 @@ import requests
 from requests.exceptions import ConnectionError
 
 
-while True:
-  try:
-    request = requests.get('http://boxadi.com')
-  except ConnectionError:
-    print('Web site does not exist')
+def ContainerManeger():
     os.system('docker system prune -y')
     os.system('docker-compose up')
-  else:
-    print('Web site exists')
-  time.sleep(60)
 
+
+while True:
+    url = 'https://boxadi.com/'
+    try:
+        x = requests.get('url')
+    except:
+        print('Web site does not exist1')
+        ContainerManeger()
+    else:
+        if x.ok:
+            print('Web site exists')
+        else:
+            ContainerManeger()
+            print('Web site does not exist2')
+    time.sleep(60)
