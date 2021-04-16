c1748d1d06316415304dcbe868d62d1708a9029b
OnStas <stanislav-helios@i.ua>
2021-04-16 15:54:38 +0300

    Update compose-autostart.py
    Added monitoring of site infrastructure and getting project from git repo.

--- compose-autostart.py
+++ compose-autostart.py
@@ -1,25 +1,24 @@
 import os
-import time
 import requests
-from requests.exceptions import ConnectionError
-
 
 def ContainerManeger():
+    print(' DEF Web site does not exist_ start CM')
+    os.system('git clone https://github.com/OnStas/Docker_php_letsencript temp')
+    os.system('mv temp/.git Docker_php_letsencript')
+    os.system('rm -rf temp')
     os.system('docker system prune -y')
-    os.system('docker-compose up')
+    os.system('docker-compose -f Docker_php_letsencript/docker-compose.yml up')
 
 
-while True:
-    url = 'https://boxadi.com/'
-    try:
-        x = requests.get('url')
-    except:
-        print('Web site does not exist1')
-        ContainerManeger()
+url = 'https://boxadi.com/'
+try:
+    x = requests.get('url')
+except:# ConnectionError:
+    print(' Web site does not exist1')
+    ContainerManeger()
+else:
+    if x.ok:
+        print(x.ok +' Web site exists')
     else:
-        if x.ok:
-            print('Web site exists')
-        else:
-            ContainerManeger()
-            print('Web site does not exist2')
-    time.sleep(60)
+         ContainerManeger()
+         print(x.res + ' Web site does not exist2')
