a7c424a00ee1041a73f380008be3a933fbc9f458
OnStas <stanislav-helios@i.ua>
2021-04-16 15:49:30 +0300

    Create first_start.py
    the first start of infrastructure, to create a Cron job better start it as sudo user

---
+++ first_start.py
@@ -0,0 +1,14 @@
+import os
+from pathlib import Path
+
+p = os.path.abspath('compose-auto.py')
+print(p)
+path = Path(p).parent.parent
+print(path)
+try:
+   os.system('mv compose-automation.py ' + str(path))
+except:
+   print('file not found')
+print('..copy done...')
+os.system('echo \"*/5 * * * * python ' + str(path) + '/compose-automation.py\" >> /etc/crontab')
+# print('echo \"*/5 * * * * python ' + str(path) + '/compose-automation.py\" >> /etc/crontab')
